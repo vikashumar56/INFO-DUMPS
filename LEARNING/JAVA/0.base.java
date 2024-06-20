437173d01c94c79da0c602aebc24cf78b177cf12
Vikash Kumar <83977152+vikashumar56@users.noreply.github.com>
2024-06-20 09:35:55 +0000

    Thu, Jun 20, 2024, 3:05 PM +05:30

---
+++ LEARNING/JAVA/0.base.java
@@ -0,0 +1,36 @@
+class base {
+
+    public static void main(String[] args) {
+        //Your Code here
+    }
+}
+
+
+//Function = block of code that performs a task
+//Class = Container for one or more related function
+//Method = Function which is part of a class
+//Access modifier = if other classes or methods can access these classes or not
+//PascalNamingConvention = Classes use this
+//camelNamingConvention = Methods use this
+
+//Code -> Compiler -> ByteCode -> JRE -> Native Code -> Output
+
+/*James Gosling 1995 at Sun Microsystems and later
+acquired by Oracle in 2010, Oak after oak tree
+outside office and later java after java coffee.*/
+
+/*Primitive data type: store simple values, and dont have any members in it contrast to reference
+        byte age = 30;
+        long viewsCount = 3_123_456_789L
+        float price = 10.99F
+        char letter = 'A'
+        boolean isEligible = false
+        short year = 2022
+        int x = 10
+        double gpa = 3.7*/
+
+/*Reference data type: to store complex objects like data, mail messages
+        Date now = new Date();
+        now.getTime()
+
+

186d363333a2e6b3260456c8374a2e324a4bb7a6
Vikash Kumar <83977152+vikashumar56@users.noreply.github.com>
2024-06-22 22:00:23 +0000

    Sun, Jun 23, 2024, 3:30 AM +05:30

--- LEARNING/JAVA/0.base.java
+++ LEARNING/JAVA/0.base.java
@@ -160,8 +160,9 @@ System.out.println(y); - returns "llo"
 Formatting Numbers
     NumberFormat currency = new NumberFormat(); - now this line is not correct since "NumberFormat" is a ABSTRACT so we cannot
     create any new instance out of it instead we write
-    NumberFormat currency = NumberFormat.getCurrencyInstance(); -some other
-    
+    NumberFormat currency = NumberFormat.getCurrencyInstance();
     String result = currency.format(number: 123.46);
+Or
+    
 
 */

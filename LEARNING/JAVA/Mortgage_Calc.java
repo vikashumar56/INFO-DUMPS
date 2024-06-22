a89b498fea9fab76e928703126a8ee16b303d657
Vikash Kumar <83977152+vikashumar56@users.noreply.github.com>
2024-06-22 22:32:08 +0000

    Sun, Jun 23, 2024, 4:02 AM +05:30

--- LEARNING/JAVA/Mortgage_Calc.java
+++ LEARNING/JAVA/Mortgage_Calc.java
@@ -1,4 +1,9 @@
-// Program to create a Mortgage calculator which will take Principal Amount, Annual Interest rate and Period in years
+/* Program to create a Mortgage calculator which will take Principal Amount, Annual Interest rate
+and Period in years as input from the user and calculate the monthly payment.
 
+Now Formula for this is M = P(1+r)^n/(1+r)^n-1
+where M is the monthly payment, P is the principal amount, r is the monthly interest rate and
+n is the number of payments.
+*/
 class Mortgage_Calc
 

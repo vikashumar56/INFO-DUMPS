bf07b999c1a41b62e06f36c154952b92a6ff71a4
Vikash Kumar <83977152+vikashumar56@users.noreply.github.com>
2024-06-30 12:52:46 +0000

    Sun, Jun 30, 2024, 6:22 PM +05:30

--- LEARNING/C+CPP/cpp/striver/10.funct.2.cpp
+++ LEARNING/C+CPP/cpp/striver/10.funct.2.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 // Pass by value takes a copy of the value and then passes it so the original value remains
-// unchanged
+//  unchanged
 /*
  * Passes a copy of the value, allowing the function to modify the copy instead of the original
  * value. This means that any changes made to the value within the function do not affect the
@@ -38,8 +38,17 @@ void swap(int &a, int &b)
     b = temp;
 }
 
+// for arrays always use pass by reference
+
+void passArray(int arr2[], int n)
+{
+    arr2[0] += 100;
+    cout << "Value inside function " << arr2[0] << endl;
+}
+
 int main()
 {
+    // pass by value
     int num = 10;
     passValue(num);
     cout << "Original value: " << num << endl;
@@ -48,11 +57,33 @@ int main()
     passString(s);
     cout << "Original string: " << s << endl;
 
+    int arr1[5];
+    for (int i = 1; i <= 5; i++)
+    {
+        cin >> arr1[i];
+    }
+
+    for (int i = 1; i <= 5; i++)
+    {
+        cout << arr1[i] << " " << endl;
+    }
+
+    // pass by reference
     int x = 5;
     int y = 10;
     cout << "Before swap: x = " << x << ", y = " << y << endl;
     swap(x, y);
     cout << "After swap: x = " << x << ", y = " << y << endl;
+
+    int n = 5;
+    int arr2[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr2[i];
+    }
+    passArray(arr2, n);
+
+    cout << "Value inside main function " << arr2[0] << endl;
 
     return 0;
 }
